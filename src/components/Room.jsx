1bea40a57eabd48bbc0dabe1bcbe9558aa874f6e
Mayank Goyal <2004mayankgoyal@gmail.com>
2025-07-28 15:45:49 +0530

    change

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import line from "../assets/Line.png";
-import item1 from "../assets/item.png";
-import item2 from "../assets/item2.png";
-import item3 from "../assets/item3.png";
-import item4 from "../assets/item4.png";
-import item5 from "../assets/item5.png";
+import item from "../assets/Item.png";
+import item2 from "../assets/Item2.png";
+import item3 from "../assets/Item3.png";
+import item4 from "../assets/Item4.png";
+import item5 from "../assets/Item5.png";
 import "../utils-css/Room.css";
 
 const Room = () => {
@@ -19,7 +19,7 @@ const Room = () => {
           <h1>LUXURY ROOMS & SUITES</h1>
         </div>
         <div className="image-upper">
-          <img src={item1} alt="" />
+          <img src={item} alt="" />
           <img src={item2} alt="" />
         </div>
         <div className="image-down">
